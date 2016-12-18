55e5ea7a2da0b562ae1ef175b1ec8593f43aae85
Jez Templeton <j.templeton@bom.gov.au>
2016-12-18 22:31:42 +1100

    More features! Commit messages are hard when you don't do them regularly.

--- src/components/Farewell/Message/Message.js
+++ src/components/Farewell/Message/Message.js
@@ -12,48 +12,29 @@ const fonts = [
   'crafty-girls'
 ]
 
-const salutations = require('./strings/salutations');
-const fragments = require('./strings/messages');
-
 export default class Message extends Component {
   static propTypes = {
     farewell: PropTypes.object.isRequired
   }
 
-  constructor(){
-    super();
-
-    this.state = {
-      message: {
-        start: "Can't believe you're leaving!",
-        middle: "I think some others want to tell you how they feel. Press the 'another' link for more!",
-        end: "Cheers,"
-      }
-    }
-  }
-  componentWillReceiveProps(nextProps){
-    this.setState({
-      message: {
-        start: sample(fragments.start),
-        middle: sample(fragments.middle),
-        end: sample(fragments.end)
-      }
-    });
-  }
-
   render() {
-    const {message, author} = this.props.farewell;
+    const {salutation, message, author} = this.props.farewell;
     const {subject} = this.props;
-    const {start, middle, end } = fragments;
 
     return (
-      <blockquote className={'container message ' + sample(fonts)}>
-        <div>
-          <p>{sample(salutations)} {subject},</p>
-          <p>{this.state.message.start}. {this.state.message.middle}. {this.state.message.end}</p>
+      <blockquote className={'container message '}>
+        <div className='message__content'>
+          <p>{salutation} {subject},</p>
+          <p>{message.start} {message.middle} {message.end}</p>
         </div>
         <footer className='author'>
-          <cite>{author}</cite>
+          <div className='author__photo'>
+            <img src={author.portrait} role="presentation" />
+          </div>
+          <cite className='author__label'>
+            <span className='author__name'>{author.name}</span>
+            <span className='author__role'>{author.role}</span>
+          </cite>
         </footer>
       </blockquote>
     );
--- src/reducers/farewell.js
+++ src/reducers/farewell.js
@@ -1,17 +1,67 @@
+import { sample, uniq } from 'lodash';
+import { detect } from 'gender-detection';
 import * as types from '../actions/types';
 
 const initialState = {
-  message: 'Something something generate more.',
-  author: 'Jez Templeton'
+  salutation: "Hey",
+  message: {
+    start: "Looks like you're finally leaving.",
+    middle: "It turns out that we couldn't fit all the messages into the card, so I made this for you. Click the 'Another!' link to see more of your farewell messages.",
+    end: "Cheers,"
+  },
+  author: {
+    name: "Jez Templeton",
+    role: "Underutilised Developer",
+    portrait: ""
+  }
 };
+
+const strings = require('../../data/strings');
+
+function generateMessage() {
+  const middle = () => {
+    let middle = [];
+
+    let x = Math.round(Math.random()*4) + 1;
+
+    for (let i = 0; i < x; i++ ){
+      middle.push(sample(strings.middle));
+    }
+
+    return uniq(middle).join(" ");
+  };
+
+  return {
+    start: sample(strings.start),
+    middle: middle(),
+    end: sample(strings.end)
+  };
+}
+
+function getRandomPortrait(name) {
+  const gender = (detect(name) === "female") ? "women" : "men";
+
+  const num =  Math.floor(Math.random() * (100 - 1) + 1);
+  return `https://randomuser.me/api/portraits/${gender}/${num}.jpg`;
+}
+
+function generateAuthor() {
+  const name = sample(strings.firstname) + " " + sample(strings.surname);
+  return {
+    name: name,
+    role: sample(strings.role),
+    portrait: getRandomPortrait(name)
+  };
+}
 
 export default (state = initialState, action) => {
     switch (action.type) {
         case types.FAREWELL_GENERATE:
             return {
               ...state,
-              message: 'Forsooth, you be leaving.',
-              author: 'Brian McBlessedface'
+              salutation: sample(strings.salutations),
+              message: generateMessage(),
+              author: generateAuthor(),
             };
         default:
           return state;
